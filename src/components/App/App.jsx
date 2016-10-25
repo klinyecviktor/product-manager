ea906aeac6f35934cfc1071f20531b3ca5b754e7
Doctor <vityok3458@gmail.com>
2016-10-25 09:40:40 +0300

    App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { AppBar } from '../';
 import RaisedButton from 'material-ui/RaisedButton';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
 
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
+function handleTouchTap() {
+    alert('onTouchTap triggered on the title component');
+}
+
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    }
+
+    handleToggle = () => {
+        console.log('handleToggle');
+        this.setState({open: !this.state.open});
+    }
+
     render() {
         const bar = (AppBar) ? '<AppBar/>' : 'No AppBar';
         console.log(bar);
@@ -16,10 +29,19 @@ export default class App extends React.Component {
         return (
             <div>
                 <MuiThemeProvider>
-                    <AppBar/>
-                </MuiThemeProvider>
-                <MuiThemeProvider>
-                    <RaisedButton label="Default" />
+                    <div>
+                        <AppBar iconElementLeft={ <IconButton iconClassName="muidocs-icon-custom-github" tooltip="GitHub"/>  }
+                                onLeftIconButtonTouchTap={ handleTouchTap }
+                                onTitleTouchTap={handleTouchTap}/>
+                        <RaisedButton
+                            label="Toggle Drawer"
+                            onTouchTap={this.handleToggle}
+                        />
+                        <Drawer open={this.state.open}>
+                            <MenuItem>Menu Item</MenuItem>
+                            <MenuItem>Menu Item 2</MenuItem>
+                        </Drawer>
+                    </div>
                 </MuiThemeProvider>
             </div>
         );
--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import { createStore } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes/routes';
 import reducers from './reducers';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const store = createStore(reducers);
 
